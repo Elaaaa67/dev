528332366d3648663645c7a15f0085c4f7586811
Ela.blt67 <Ela.blt2767@gmail.com>
2023-05-16 09:21:21 +0200

    v2

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -4,3 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index),
 ]
+def bonjour(request):
+    nom=request.GET["nom"] # récupère la valeur du paramètre nom du formulaire
+    return render(request,'myfirstapp/bonjour.html',{"nom":nom}) # passe cette valeur à la vue au travers du dictionnaire de contexte
